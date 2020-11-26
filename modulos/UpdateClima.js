3679027e447df0f38f0771a69dab579fdde6c4cb
Alan Silva <60674021+AlanMSilva@users.noreply.github.com>
2020-11-26 15:49:03 -0600

    Add files via upload

---
+++ modulos/UpdateClima.js
@@ -0,0 +1,19 @@
+const request = require('postman-request');
+const statusClima = (city, callback)=>{
+  let urlLatLon = `http://api.weatherstack.com/current?access_key=a34cfae803604bdab8806c48e319f90d&query=${city}&format=json`;
+
+  request(urlLatLon, (error,response,body)=>{
+  if(error)
+  {
+    callback('Ocurrio un error en la Busqueda', undefined);
+  }
+  else
+  {
+    let data = JSON.parse(body);
+    callback(undefined,{
+      temperature: data.current.temperature
+    });
+  }
+});
+}
+module.exports = statusClima;
